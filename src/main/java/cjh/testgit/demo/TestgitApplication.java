c978abda7eaebdfb1151e500475aad1e9b4f2bb3
GodStrength100 <cyk920@qq.com>
2019-07-26 09:36:59 +0800

    init

---
+++ src/main/java/cjh/testgit/demo/TestgitApplication.java
@@ -0,0 +1,13 @@
+package cjh.testgit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestgitApplication.class, args);
+    }
+
+}
